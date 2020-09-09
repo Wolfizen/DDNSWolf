b871f413f92febad0de47868e298ee2826fc2958
Wolfizen <wolfizen@wolfizen.net>
2020-09-08 23:43:42 -0700

    Fix bug where address updates were being sorted without comparison implemented

--- ddnswolf/models/address_update.py
+++ ddnswolf/models/address_update.py
@@ -1,3 +1,4 @@
+from functools import total_ordering
 from ipaddress import IPv4Address, IPv6Address
 
 from dns.rdatatype import RdataType
@@ -16,14 +17,28 @@ class AddressUpdate:
         pass
 
 
+@total_ordering
 class IPv4AddressUpdate(AddressUpdate):
     """An update for an Internet Protocol version 4 address."""
+
+    rdtype = RdataType.A
 
     def __init__(self, address: IPv4Address):
         super(IPv4AddressUpdate, self).__init__()
         self.address = address
-        self.rdtype = RdataType.A
         """The DNS record type that represents this kind of address."""
+
+    def __eq__(self, other):
+        if isinstance(other, IPv4AddressUpdate):
+            return self.address == other.address
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, IPv4AddressUpdate):
+            return self.address < other.address
+        elif isinstance(other, IPv4AddressUpdate):
+            return False
+        return NotImplemented
 
     def __str__(self):
         return str(self.address)
@@ -32,14 +47,28 @@ class IPv4AddressUpdate(AddressUpdate):
         return f"{type(self).__name__}({self.address!r})"
 
 
+@total_ordering
 class IPv6AddressUpdate(AddressUpdate):
     """An update for an Internet Protocol version 6 address."""
+
+    rdtype = RdataType.AAAA
 
     def __init__(self, address: IPv6Address):
         super(IPv6AddressUpdate, self).__init__()
         self.address = address
-        self.rdtype = RdataType.AAAA
         """The DNS record type that represents this kind of address."""
+
+    def __eq__(self, other):
+        if isinstance(other, IPv6AddressUpdate):
+            return self.address == other.address
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, IPv6AddressUpdate):
+            return self.address < other.address
+        elif isinstance(other, IPv4AddressUpdate):
+            return False
+        return NotImplemented
 
     def __str__(self):
         return str(self.address)
