b47061c0c48f2241eee85cb6b21853a0035690f2
Wolfizen <wolfizen@wolfizen.net>
2020-10-01 23:53:31 -0700

    Add readme, upgrade distribution format to be setup.cfg based

---
+++ README.md
@@ -0,0 +1,26 @@
+# DDNSWolf
+
+See `LICENSE.md` for your license to use this software.
+
+
+## Using DDNSWolf
+
+WIP
+
+## Modifying DDNSWolf
+
+### Building for development
+
+Create a virtual environment, if desired.
+
+    python3 -m virtualenv ./venv
+    . ./venv/bin/activate
+
+Install dependencies. There is no `requirements.txt` file, instead
+dependencies are declared in `setup.cfg` and pip can read this file.
+
+    pip install .
+
+### Building for distribution
+
+WIP
--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
 #!/bin/env python3
-# https://setuptools.readthedocs.io/en/latest/
+# Setuptools stub. See setup.cfg
 
-from setuptools import setup, find_namespace_packages
+import setuptools
 
 
-setup(
-    name="ddnswolf",
-    version="0",
-    author="Wolfizen",
-    author_email="wolfizen@wolfizen.net",
-    description="Dynamic DNS updater",
-    url="https://wolfizen.net/ddnswolf",
-    license="GPLv3+",
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-    ],
-    packages=find_namespace_packages(include=["ddnswolf.*"]),
-)
+setuptools.setup()
