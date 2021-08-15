8a24b56cea90f113f96527c26ce4bdb4081bffee
Wolfizen <wolfizen@wolfizen.net>
2021-08-14 17:56:28 -0700

    Release version 3

--- ddnswolf/version.py
+++ ddnswolf/version.py
@@ -14,7 +14,7 @@ from pkg_resources import parse_version
 logger = logging.getLogger(__name__)
 
 
-release_version = "2"
+release_version = "3"
 """
 Public release number. This is used to identify releases, package versions, etc. This
 is incremented every time the project is ready for a public release. The first commit
